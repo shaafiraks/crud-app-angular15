59a2395520b320d343b47aa629cdc21210a9f774
shaafiraks <shafiraaks@gmail.com>
2023-03-28 15:58:43 +0700

    header

---
+++ crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,51 @@
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss']
+})
+export class EmpAddEditComponent {
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate',
+  ]
+
+  constructor(private _fb: FormBuilder, private _empService: EmployeeService, private _dialogRef: DialogRef<EmpAddEditComponent>){
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    })
+  }
+
+  onFormSubmit(){
+    if(this.empForm.valid){
+      console.log(this.empForm.value)
+      this._empService.addEmployee(this.empForm.value).subscribe({
+        next: (val: any) => {
+          alert("Employee added successfully");
+          this._dialogRef.close()
+        },
+        error: (err: any) => {
+          console.log(err);
+        },
+      })
+    }
+  }
+
+}
